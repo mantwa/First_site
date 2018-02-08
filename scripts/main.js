c59292f0b4ecb8f718c9b5ab8d0a24a0549de3cf
mantwa <36259340+mantwa@users.noreply.github.com>
2018-02-08 12:26:04 +0200

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -14,7 +14,7 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
   var myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
-  myHeading.textContent = 'masego and coding, ' + myName;
+  myHeading.textContent = 'Masego Shyleen Makgasane, ' + myName;
 }
 
 
@@ -22,7 +22,7 @@ function setUserName() {
     setUserName();
   } else {
     var storedName = localStorage.getItem('name');
-    myHeading.textContent = 'masego and coding, ' + storedName;
+    myHeading.textContent = 'Masego Shyleen Makgasane, ' + storedName;
   }
 
   myButton.onclick = function() {
